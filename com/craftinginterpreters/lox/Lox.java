2315b5bbc3bbfb89a51e1d0a02808f1df6c49eb3
Janus Troelsen <janus@flipstone.com>
2023-04-30 16:01:57 -0500

    chapter 6: parsing expressions

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -40,11 +40,18 @@ public class Lox {
   private static void run(String source) {
     Scanner scanner = new Scanner(source);
     List<Token> tokens = scanner.scanTokens();
+    //// For now, just print the tokens.
+    //for (Token token : tokens) {
+    //  System.out.println(token);
+    //}
 
-    // For now, just print the tokens.
-    for (Token token : tokens) {
-      System.out.println(token);
-    }
+    Parser parser = new Parser(tokens);
+    Expr expression = parser.parse();
+
+    // Stop if there was a syntax error.
+    if (hadError) return;
+
+    System.out.println(new AstPrinter().print(expression));
   }
   static void error(int line, String message) {
     report(line, "", message);
@@ -55,6 +62,14 @@ public class Lox {
     System.err.println(
         "[line " + line + "] Error" + where + ": " + message);
     hadError = true;
+  }
+
+  static void error(Token token, String message) {
+    if (token.type == TokenType.EOF) {
+      report(token.line, " at end", message);
+    } else {
+      report(token.line, " at '" + token.lexeme + "'", message);
+    }
   }
   static boolean hadError = false;
 }
