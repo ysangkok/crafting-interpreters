6d4d84dab1ec20f6fd05c29a57e5a382e00238b4
Janus Troelsen <ysangkok@gmail.com>
2023-05-06 19:46:54 -0500

    chapter 7

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -24,6 +24,7 @@ public class Lox {
     run(new String(bytes, Charset.defaultCharset()));
     // Indicate an error in the exit code.
     if (hadError) System.exit(65);
+    if (hadRuntimeError) System.exit(70);
   }
   private static void runPrompt() throws IOException {
     InputStreamReader input = new InputStreamReader(System.in);
@@ -52,6 +53,7 @@ public class Lox {
     if (hadError) return;
 
     System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(expression);
   }
   static void error(int line, String message) {
     report(line, "", message);
@@ -71,5 +73,13 @@ public class Lox {
       report(token.line, " at '" + token.lexeme + "'", message);
     }
   }
+
+  static void runtimeError(RuntimeError error) {
+    System.err.println(error.getMessage() +
+        "\n[line " + error.token.line + "]");
+    hadRuntimeError = true;
+  }
   static boolean hadError = false;
+  static boolean hadRuntimeError = false;
+  private static final Interpreter interpreter = new Interpreter();
 }
