ecd600096b4b007a42d5beeedc4033208c95c39e
Janus Troelsen <ysangkok@gmail.com>
2023-05-22 19:44:35 -0500

    chapter 8

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -47,13 +47,13 @@ public class Lox {
     //}
 
     Parser parser = new Parser(tokens);
-    Expr expression = parser.parse();
+    List<Stmt> statements = parser.parse();
 
     // Stop if there was a syntax error.
     if (hadError) return;
 
-    System.out.println(new AstPrinter().print(expression));
-    interpreter.interpret(expression);
+    //System.out.println(new AstPrinter().print(expression));
+    interpreter.interpret(statements);
   }
   static void error(int line, String message) {
     report(line, "", message);
