f93e68f29b2c25756643febcc250f5f192fb619f
Janus Troelsen <ysangkok@gmail.com>
2023-08-01 00:52:49 -0500

    chapter 21 global variables (w/o assignment)

--- src/main.zig
+++ src/main.zig
@@ -247,6 +247,15 @@ const Value = union(ValueTag) {
     string: []u8,
 };
 
+fn printValue(stdout: anytype, val: Value) !void {
+    switch (val) {
+        .number => |num| try stdout.print("printValue: {d}\n", .{num}),
+        .boolean => |boo| try stdout.print("printValue: {}\n", .{boo}),
+        .string => |str| try stdout.print("printValue: {s}\n", .{str}),
+        .nil => try stdout.print("printValue: nil\n", .{}),
+    }
+}
+
 fn numberP(currentChunk: *Chunk, parser: *Parser, buf: []const u8) void {
     const value = std.fmt.parseFloat(f64, buf[parser.previous.start .. parser.previous.start + parser.previous.length]) catch {
         @panic("invalid");
@@ -309,6 +318,76 @@ const Prec = enum {
 
 fn expression(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner: *Scanner, buf: []const u8) void {
     parsePrecedence(gpa, currentChunk, parser, scanner, buf, Prec.ASSIGNMENT);
+}
+
+fn declaration(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner: *Scanner, buf: []const u8) !void {
+    if (matchP(TokenType.VAR, parser, scanner, buf)) {
+        try varDeclaration(gpa, currentChunk, parser, scanner, buf);
+    } else {
+        try statement(gpa, currentChunk, parser, scanner, buf);
+    }
+}
+
+fn varDeclaration(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner: *Scanner, buf: []const u8) !void {
+    var global = try parseVariable(gpa, currentChunk, parser, scanner, buf, "Expect variable name.");
+
+    if (matchP(TokenType.EQUAL, parser, scanner, buf)) {
+        expression(gpa, currentChunk, parser, scanner, buf);
+    } else {
+        try currentChunk.data.append(@intFromEnum(Op.NIL));
+    }
+    consumeP(parser, scanner, buf, TokenType.SEMICOLON, "Expect ';' after variable declaration.");
+
+    try defineVariable(global, currentChunk);
+}
+
+fn parseVariable(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner: *Scanner, buf: []const u8, errorMessage: []const u8) !usize {
+    consumeP(parser, scanner, buf, TokenType.IDENTIFIER, errorMessage);
+    return try identifierConstant(gpa, parser.previous, currentChunk, buf);
+}
+
+fn identifierConstant(gpa: std.mem.Allocator, name: Token, currentChunk: *Chunk, buf: []const u8) !u8 {
+    var dst = try gpa.alloc(u8, name.length);
+    std.mem.copy(u8, dst, buf[name.start .. name.start + name.length]);
+    var val: Value = Value{ .string = dst };
+    try currentChunk.constants.append(val);
+    return @truncate(u8, currentChunk.constants.items.len - 1);
+}
+
+fn defineVariable(global: usize, currentChunk: *Chunk) !void {
+    try currentChunk.data.append(@intFromEnum(Op.DEFINE_GLOBAL));
+    try currentChunk.data.append(@truncate(u8, global));
+}
+
+fn check(typ: TokenType, parser: *Parser) bool {
+    return parser.current.typ == typ;
+}
+
+fn matchP(typ: TokenType, parser: *Parser, scanner: *Scanner, buf: []const u8) bool {
+    if (!check(typ, parser)) return false;
+    advanceP(parser, scanner, buf);
+    return true;
+}
+
+fn statement(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner: *Scanner, buf: []const u8) !void {
+    if (matchP(TokenType.PRINT, parser, scanner, buf)) {
+        //std.debug.print("print\n", .{});
+        try printStatement(gpa, currentChunk, parser, scanner, buf);
+    } else {
+        try expressionStatement(gpa, currentChunk, parser, scanner, buf);
+    }
+}
+
+fn expressionStatement(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner: *Scanner, buf: []const u8) !void {
+    expression(gpa, currentChunk, parser, scanner, buf);
+    consumeP(parser, scanner, buf, TokenType.SEMICOLON, "Expect ';' after expression.");
+    try currentChunk.data.append(@intFromEnum(Op.POP));
+}
+
+fn printStatement(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner: *Scanner, buf: []const u8) !void {
+    expression(gpa, currentChunk, parser, scanner, buf);
+    consumeP(parser, scanner, buf, TokenType.SEMICOLON, "Expect ';' after value.");
+    try currentChunk.data.append(@intFromEnum(Op.PRINT));
 }
 
 fn getPrecedence(tokenType: TokenType) Prec {
@@ -400,15 +479,13 @@ fn prefix(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner
         .NUMBER => numberP(currentChunk, parser, buf),
         .FALSE, .NIL, .TRUE => literalP(currentChunk, parser),
         .STRING => stringP(gpa, currentChunk, parser, buf),
+        .IDENTIFIER => variable(gpa, currentChunk, parser, buf) catch {},
         else => panicToken(tokenType),
 
         //// One or two character tokens.
         //.Bang => try self.unary(),
         //.Equal, .BangEqual, .EqualEqual, .Greater, .GreaterEqual => try self.prefixError(),
         //.Less, .LessEqual => try self.prefixError(),
-
-        //// Literals.
-        //.Identifier => try self.variable(canAssign),
 
         //// Keywords.
         //.Nil, .True, .False => try self.literal(),
@@ -417,6 +494,16 @@ fn prefix(gpa: std.mem.Allocator, currentChunk: *Chunk, parser: *Parser, scanner
         //.And, .Class, .Else, .For, .Fun, .If, .Or => try self.prefixError(),
         //.Print, .Return, .Var, .While, .Error, .Eof => try self.prefixError(),
     }
+}
+
+fn variable(gpa: std.mem.Allocator, chunk: *Chunk, parser: *Parser, buf: []const u8) !void {
+    try namedVariable(parser.previous, gpa, chunk, buf);
+}
+
+fn namedVariable(name: Token, gpa: std.mem.Allocator, currentChunk: *Chunk, buf: []const u8) !void {
+    var arg = try identifierConstant(gpa, name, currentChunk, buf);
+    try currentChunk.data.append(@intFromEnum(Op.GET_GLOBAL));
+    try currentChunk.data.append(arg);
 }
 
 fn panicToken(tokenType: TokenType) noreturn {
@@ -515,16 +602,16 @@ pub fn main() !void {
     var scanner = Scanner{ .start = 0, .current = 0, .line = 1 };
     var compilingChunk = Chunk{ .data = std.ArrayList(u8).init(gpa), .constants = std.ArrayList(Value).init(gpa), .line = 1 };
     var parser: Parser = undefined;
-    advanceP(&parser, &scanner, file_buffer);
-    expression(gpa, &compilingChunk, &parser, &scanner, file_buffer);
-    try compilingChunk.data.append(@intFromEnum(Op.RETURN));
-    consumeP(&parser, &scanner, file_buffer, TokenType.EOF, "Expect end of expression.");
 
-    //while (!isAtEnd(&scanner, file_buffer)) {
-    //    const token = scanToken(&scanner, file_buffer);
-    //    try stdout.print("read token {s}\n", .{@tagName(token.typ)});
-    //}
-    //try stdout.print("scanner current={d} buf.len={d}\n", .{ scanner.current, file_buffer.len });
+    advanceP(&parser, &scanner, file_buffer);
+    while (true) {
+        if (parser.current.typ == TokenType.EOF) {
+            break;
+        }
+        //try stdout.print("declaration? {}\n", .{parser.current.typ});
+        try bw.flush();
+        try declaration(gpa, &compilingChunk, &parser, &scanner, file_buffer);
+    }
 
     {
         var chunks = std.MultiArrayList(Chunk){};
@@ -672,6 +759,8 @@ fn runChunk(gpa: std.mem.Allocator, chunk: *Chunk, vm: *VM) !void {
     var bw = std.io.bufferedWriter(stdout_file);
     const stdout = bw.writer();
 
+    var globals = std.StringHashMap(Value).init(gpa);
+
     var lst: Op = undefined;
     var ip: usize = 0;
     while (true) {
@@ -679,6 +768,7 @@ fn runChunk(gpa: std.mem.Allocator, chunk: *Chunk, vm: *VM) !void {
             break;
         }
         const op = @enumFromInt(Op, chunk.data.items[ip]);
+        try stdout.print("executing ip={d}\n", .{ip});
         ip += 1;
         lst = op;
         switch (op) {
@@ -755,6 +845,7 @@ fn runChunk(gpa: std.mem.Allocator, chunk: *Chunk, vm: *VM) !void {
             },
             .ADD => {
                 try stdout.print("add: original stackTop={d}\n", .{vm.stackTop});
+                try bw.flush();
                 const b = vm.stack[vm.stackTop - 0];
                 const a = vm.stack[vm.stackTop - 1];
                 switch (b) {
@@ -770,15 +861,55 @@ fn runChunk(gpa: std.mem.Allocator, chunk: *Chunk, vm: *VM) !void {
                         var new = try concatAndReturnBuffer(gpa, as, bs);
                         vm.stack[vm.stackTop] = Value{ .string = new };
                     },
-                    else => {
-                        @panic("invalid types for + operator");
+                    .nil => {
+                        @panic("invalid nil for + operator");
+                    },
+                    .boolean => |_| {
+                        @panic("invalid bool for + operator");
                     },
                 }
             },
+            .PRINT => {
+                try stdout.print("executing PRINT\n", .{});
+                try printValue(stdout, vm.stack[vm.stackTop]);
+                vm.stackTop -= 1;
+            },
+            .POP => {
+                try stdout.print("popping stackTop from {d} to {d}\n", .{ vm.stackTop, vm.stackTop - 1 });
+                vm.stackTop -= 1;
+            },
+            .DEFINE_GLOBAL => {
+                const name = try get_string(chunk.constants.items[@as(usize, chunk.data.items[ip])]);
+                ip += 1;
+                //try stdout.print("defining global {s}\n", .{name});
+                //try printValue(stdout, vm.stack[vm.stackTop]);
+                try globals.putNoClobber(name, vm.stack[vm.stackTop]);
+                vm.stackTop -= 1;
+                try stdout.print("defining global {s}, decresed stack from {d} (read from here) to {d}\n", .{ name, vm.stackTop + 1, vm.stackTop });
+            },
+            .GET_GLOBAL => {
+                const constidx = @as(usize, chunk.data.items[ip]);
+                ip += 1;
+                const name = try get_string(chunk.constants.items[constidx]);
+                try stdout.print("getting global using constant idx {d}. It is {s}\n", .{ constidx, name });
+                var val = globals.get(name);
+                if (val) |v| {
+                    vm.stackTop += 1;
+                    try stdout.print("setting stack index {d}\n", .{vm.stackTop});
+                    vm.stack[vm.stackTop] = v;
+                    //try stdout.print("stack elem 0: ", .{});
+                    //try printValue(stdout, vm.stack[0]);
+                    //try stdout.print("stack elem 1: ", .{});
+                    //try printValue(stdout, vm.stack[1]);
+                    //try stdout.print("stack elem 2: ", .{});
+                    //try printValue(stdout, vm.stack[2]);
+                    //try stdout.print("stack elem 3: ", .{});
+                    //try printValue(stdout, vm.stack[3]);
+                } else {
+                    @panic("global not found");
+                }
+            },
         }
-    }
-    if (lst != .RETURN) {
-        @panic("expected return");
     }
     try bw.flush();
 }
@@ -821,6 +952,10 @@ const Op = enum(u8) {
     NOT,
     NEGATE,
     RETURN,
+    PRINT,
+    POP,
+    DEFINE_GLOBAL,
+    GET_GLOBAL,
 };
 
 fn demoChunks(gpa: std.mem.Allocator) !std.MultiArrayList(Chunk) {
